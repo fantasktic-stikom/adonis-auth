a7114b026673e9fcad7c1e9614cde3c37eacf4a7
Ryan Adhitama Putra <ryanadhitama2@gmail.com>
2021-07-16 22:10:53 +0800

    middleware

--- start/routes.js
+++ start/routes.js
@@ -18,11 +18,18 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-Route.get('register', 'Auth/RegisterController.index').as('register.index')
-Route.post('register', 'Auth/RegisterController.store').as('register.store')
 
-Route.get('login', 'Auth/LoginController.index').as('login.index')
-Route.post('login', 'Auth/LoginController.check').as('login.check')
-Route.get('logout', 'Auth/LoginController.logout').as('logout')
 
-Route.get('dashboard', 'DashboardController.index').as('dashboard')
+Route.group(() => {
+    Route.get('register', 'Auth/RegisterController.index').as('register.index')
+    Route.post('register', 'Auth/RegisterController.store').as('register.store')
+
+    Route.get('login', 'Auth/LoginController.index').as('login.index')
+    Route.post('login', 'Auth/LoginController.check').as('login.check')
+}).middleware(['RedirectIfAuthenticated'])
+
+
+Route.group(() => {
+    Route.get('dashboard', 'DashboardController.index').as('dashboard')
+    Route.get('logout', 'Auth/LoginController.logout').as('logout')
+}).middleware(['Authenticate'])
